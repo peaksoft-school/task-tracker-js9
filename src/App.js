60580cfa3c7405a0868f7eccec0546d54cbfc060
airises <isaevasai7@gmail.com>
2023-07-10 19:13:50 +0600

    created Date Picker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { DataPickers } from './components/UI/data-picker/DataPicker'
+
 function App() {
-   return <div>Task Tracker js-9</div>
+   return (
+      <div>
+         Task Tracker js-9
+         <DataPickers />
+      </div>
+   )
 }
 
 export default App
