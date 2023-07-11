5ef8136a3a6a9a5b4f123652d90a1daa3fe0e598
Nurzhanov-Bekzat <bekzatnurzhanov888@gmail.com>
2023-07-11 15:31:09 +0600

    import { TextField, styled } from '@mui/material' import React, { forwardRef } from 'react'
    export const Input = forwardRef(
       ({ type, borderRadius, placeholder, ...props }, ref) => {
          return (
             <StyledInput
                type={type}
                ref={ref}
                placeholder={placeholder}
                {...props}
             />
          )
       }
    )
    
    const StyledInput = styled(TextField)((props) => ({
       width: props.width,
       marginLeft: '25%',
       marginTop: '100px',
       border: props.border,
       '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
          height: props.height,
          padding: props.padding,
       },
       '& .css-1qi90xi-MuiInputBase-root-MuiOutlinedI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import { Input } from './components/UI/input/Input'
-
 function App() {
-   return (
-      <div>
-         <Input
-            placeholder="Add a description"
-            type="text"
-            padding="8px 16px"
-            height="5.18rem"
-            width="42.68rem"
-            borderRadiuss="8px"
-            border="1px solid #D0D0D0;"
-         />
-      </div>
-   )
+   return <div>Task Tracker</div>
 }
 export default App
