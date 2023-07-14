53a5ea2b39c6b1c9df34ae98dda7a012c7a17bff
airises <isaevasai7@gmail.com>
2023-07-14 13:35:59 +0600

    created Input

---
+++ src/components/UI/input/Input.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+
+export function Input({
+   label,
+   type,
+   id,
+   value,
+   onChange,
+   placeholder,
+   ...other
+}) {
+   return (
+      <MyStyledInput
+         size="small"
+         label={label}
+         type={type}
+         id={id}
+         value={value}
+         onChange={onChange}
+         placeholder={placeholder}
+         {...other}
+      />
+   )
+}
+
+const MyStyledInput = styled(TextField)(() => ({
+   input: {
+      padding: 0,
+      width: 0,
+      backgroundColor: 'white',
+      borderRadius: 13,
+      height: 0,
+   },
+
+   '& label.Mui-focused': {
+      color: '#6d6d6d',
+   },
+   '& label': {
+      fontSize: '1rem',
+      fontFamily: 'CarePro',
+   },
+   '& .MuiInput-underline:after': {
+      borderBottomColor: 'green',
+   },
+   '& .MuiInputBase-input-MuiOutlinedInput-input:-webkit-autofill': {
+      borderRadius: 13,
+   },
+   '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+         border: '1px solid #D0D0D0',
+         borderRadius: 13,
+      },
+      '&:hover fieldset': {
+         border: '1px solid #0079BF',
+      },
+      '&.Mui-focused fieldset': {
+         border: '1px solid #a3a3a3',
+      },
+   },
+}))
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { ThemeProvider } from '@mui/material'
