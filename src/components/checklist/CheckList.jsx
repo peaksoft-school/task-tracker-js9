c5efaa65f4f98dda5ef01da99d2a47a943cc62ca
Ali <alisamatov106@gmail.com>
2023-07-19 19:04:42 +0600

    created checkList

--- src/components/checklist/CheckList.jsx
+++ src/components/checklist/CheckList.jsx
@@ -4,72 +4,27 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import { Input } from '../UI/input/Input'
 import { Button } from '../UI/button/Button'
-import { UpIcon, DownIcon } from '../../assets/icons'
-
-const ChecklistContainer = styled('div')({
-   border: '1px solid #ccc',
-   borderRadius: '5px',
-   padding: '10px',
-   marginBottom: '10px',
-})
-
-const ChecklistHeader = styled('div')({
-   display: 'flex',
-   alignItems: 'center',
-   marginBottom: '10px',
-})
-
-const Title = styled('p')({
-   margin: '0 10px',
-})
-
-const ProgressLine = styled('div')({
-   height: '10px',
-   borderRadius: '5px',
-   backgroundColor: '#ccc',
-})
-
-const ProgressBar = styled('div')(({ progress }) => ({
-   height: '100%',
-   borderRadius: '5px',
-   backgroundColor: '#007bff',
-   width: `${progress}%`,
-}))
-
-const ProgressLabel = styled('span')({
-   position: 'absolute',
-   top: '50%',
-   left: '50%',
-   transform: 'translate(-50%, -50%)',
-   fontSize: '12px',
-})
-
-const ItemContainer = styled('div')({
-   display: 'flex',
-   alignItems: 'center',
-   marginBottom: '10px',
-})
-
-const ItemText = styled('p')({
-   margin: '0 10px',
-})
-
-const ActionButtonsContainer = styled('div')({
-   display: 'flex',
-   justifyContent: 'flex-end',
-})
+import { UpIcon, DownIcon, PlusIcon } from '../../assets/icons'
 
 export const CheckList = ({ title }) => {
    const [showInputs, setShowInputs] = useState(false)
    const [items, setItems] = useState([])
    const [idCounter, setIdCounter] = useState(0)
    const [newItemValue, setNewItemValue] = useState('')
+   const [taskCount, setTaskCount] = useState(0)
+   const [isTaskListVisible, setTaskListVisible] = useState(false)
+   const [open, setOpen] = useState(false)
+   const maxTaskCount = 5
 
    const handleNewItemChange = (e) => {
       setNewItemValue(e.target.value)
    }
 
    const addItem = () => {
+      if (taskCount >= maxTaskCount) {
+         return
+      }
+
       const newItem = {
          id: idCounter,
          value: newItemValue,
@@ -78,19 +33,20 @@ export const CheckList = ({ title }) => {
       setItems([...items, newItem])
       setIdCounter(idCounter + 1)
       setNewItemValue('')
-      setShowInputs(false)
+      setTaskCount(taskCount + 1)
+      setTaskListVisible(true)
    }
 
+   const toggleInputs = () => {
+      setOpen((prev) => !prev)
+      if (!showInputs && items.length > 0) {
+         setTaskListVisible(true)
+      }
+   }
    const cancelAddItem = () => {
       setNewItemValue('')
-      setShowInputs(false)
-   }
-
-   const handleItemChange = (id, value) => {
-      const updatedItems = items.map((item) =>
-         item.id === id ? { ...item, value } : item
-      )
-      setItems(updatedItems)
+      setShowInputs((prev) => !prev)
+      // setOpen(false)
    }
 
    const toggleCompleted = (id) => {
@@ -103,6 +59,10 @@ export const CheckList = ({ title }) => {
    const removeItem = (id) => {
       const updatedItems = items.filter((item) => item.id !== id)
       setItems(updatedItems)
+      setTaskCount(taskCount - 1)
+      if (taskCount <= 1) {
+         setTaskListVisible(false)
+      }
    }
 
    const getProgress = () => {
@@ -114,66 +74,231 @@ export const CheckList = ({ title }) => {
       return Math.floor((completedCount / items.length) * 100)
    }
 
-   const toggleInputs = () => {
-      setShowInputs(!showInputs)
-   }
-
    return (
       <ChecklistContainer>
-         <ChecklistHeader>
-            <IconButton onClick={toggleInputs}>
-               {showInputs ? <DownIcon /> : <UpIcon />}
-            </IconButton>
-            <IconButton>
-               <EditIcon />
-            </IconButton>
-            <Title>{title}</Title>
-         </ChecklistHeader>
-         <ProgressLine>
-            <ProgressBar progress={getProgress()} />
-            <ProgressLabel>{getProgress()}%</ProgressLabel>
-         </ProgressLine>
-         {showInputs ? (
-            <>
-               <ItemContainer>
-                  <Checkbox checked={false} />
-                  <StyledInput
-                     type="text"
-                     value={newItemValue}
-                     onChange={handleNewItemChange}
-                  />
-               </ItemContainer>
-               <ActionButtonsContainer>
-                  <Button onClick={addItem}>Add</Button>
-                  <Button onClick={cancelAddItem}>Cancel</Button>
-               </ActionButtonsContainer>
-            </>
-         ) : (
-            <div>
-               <Button onClick={toggleInputs}>Add New Item</Button>
-            </div>
-         )}
-         {items.map((item) => (
-            <ItemContainer key={item.id}>
-               <Checkbox
-                  checked={item.completed}
-                  onChange={() => toggleCompleted(item.id)}
-               />
-               <ItemText>{item.value}</ItemText>
-               <IconButton onClick={() => removeItem(item.id)}>
+         <CheckListHeaderContainer>
+            <ChecklistHeader>
+               <CheckListBox>
+                  <StyledIconButton onClick={cancelAddItem}>
+                     {showInputs ? <DownIcon /> : <UpIcon />}
+                  </StyledIconButton>
+                  <StyledIconButton>
+                     <EditIcon />
+                  </StyledIconButton>
+                  <Title>{title}</Title>
+               </CheckListBox>
+               <DeleteBox>
                   <DeleteIcon />
-               </IconButton>
-            </ItemContainer>
-         ))}
+                  <p>Delete</p>
+               </DeleteBox>
+            </ChecklistHeader>
+            <ProgressContainer>
+               <TaskCountContainer>
+                  <p>
+                     {taskCount}/{maxTaskCount}
+                  </p>
+               </TaskCountContainer>
+               <ProgressLine>
+                  <ProgressBar progress={getProgress()} />
+               </ProgressLine>
+               <ProgressLabel>{getProgress()}%</ProgressLabel>
+            </ProgressContainer>
+         </CheckListHeaderContainer>
+
+         {showInputs ? (
+            <Main>
+               <div>
+                  {isTaskListVisible && (
+                     <div className="map-item">
+                        {items.map((item) => (
+                           <ItemContainer key={item.id}>
+                              <Checkbox
+                                 checked={item.completed}
+                                 onChange={() => toggleCompleted(item.id)}
+                              />
+                              <ItemText>{item.value}</ItemText>
+                              <StyledIconButton
+                                 onClick={() => removeItem(item.id)}
+                              >
+                                 <DeleteIcon />
+                              </StyledIconButton>
+                           </ItemContainer>
+                        ))}
+                     </div>
+                  )}
+               </div>
+               {open ? (
+                  <div style={{ marginTop: '3rem' }}>
+                     <ItemContainer>
+                        <Checkbox checked={false} />
+                        <StyledInput
+                           type="text"
+                           value={newItemValue}
+                           onChange={handleNewItemChange}
+                           placeholder="Введите задачу"
+                        />
+                     </ItemContainer>
+                     <ActionButtonsContainer>
+                        <CancelButton onClick={toggleInputs}>
+                           Cancel
+                        </CancelButton>
+                        <AddButton onClick={addItem}>Add</AddButton>
+                     </ActionButtonsContainer>
+                  </div>
+               ) : (
+                  <AddnewButton onClick={toggleInputs}>
+                     <PlusIcon /> Add an items
+                  </AddnewButton>
+               )}
+            </Main>
+         ) : null}
       </ChecklistContainer>
    )
 }
 
 const StyledInput = styled(Input)({
    input: {
-      display: 'none',
+      fontSize: '14px',
+      width: '39.9931rem',
+      height: '5.5rem',
+      borderRadius: '5rem',
+      padding: '0.5rem 1rem 0.5rem 1rem',
+      border: '0625rem',
    },
-   root: {
-      width: 'auto',
+})
+
+const ChecklistContainer = styled('div')({
+   width: '100%',
+   height: '100vh',
+   borderRadius: '5px',
+   padding: '10px',
+   marginBottom: '10px',
+
+   '.map-item': {
+      marginTop: '4rem',
    },
+})
+
+const ChecklistHeader = styled('div')({
+   display: 'flex',
+   justifyContent: 'space-between',
+   alignItems: 'center',
+})
+
+const Title = styled('p')({
+   margin: '0 10px',
+   fontWeight: 'bold',
+   fontSize: '16px',
+})
+
+const ProgressContainer = styled('div')({
+   display: 'flex',
+   alignItems: 'center',
+   marginTop: '0.5rem',
+   borderRadius: '0.5rem',
+})
+
+const CheckListHeaderContainer = styled('div')({
+   width: '41.875rem',
+   height: '1.1875rem',
+})
+
+const ProgressLine = styled('div')({
+   height: '0.8rem',
+   borderRadius: '5px',
+   backgroundColor: '#ccc',
+   flex: 1,
+})
+
+const ProgressBar = styled('div')(({ progress }) => ({
+   borderRadius: '5px',
+   backgroundColor: '#007bff',
+   width: `${progress}%`,
+   height: '0.8rem',
+}))
+
+const ProgressLabel = styled('span')({
+   fontSize: '1rem',
+})
+
+const TaskCountContainer = styled('div')({
+   display: 'flex',
+   justifyContent: 'center',
+})
+
+const CheckListBox = styled('div')({
+   display: 'flex',
+   alignItems: 'center',
+   gap: '',
+})
+
+const ItemContainer = styled('div')({
+   display: 'flex',
+   alignItems: 'center',
+   width: '41.875rem',
+   border: '0.0625rem',
+})
+
+const ItemText = styled('p')({
+   margin: '0 10px',
+   flex: 1,
+   fontSize: '14px',
+})
+
+const ActionButtonsContainer = styled('div')({
+   display: 'flex',
+   justifyContent: 'flex-end',
+   marginTop: '10px',
+})
+
+const StyledIconButton = styled(IconButton)({
+   height: '1.875rem',
+   borderRadius: '0.5rem',
+   border: '0.0625rem',
+   gap: '0.25rem',
+})
+
+const Main = styled('div')({
+   height: '15vh',
+   width: '41.1875rem',
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-between',
+})
+
+const DeleteBox = styled('div')({
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   gap: '0.5rem',
+})
+
+const AddButton = styled(Button)({
+   width: '4.1638rem',
+   height: '1.875rem',
+   borderRadius: '1.5rem',
+   fontFamily: 'CarePro',
+   fontSize: '0.875rem',
+   fontWeight: '400',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+})
+
+const CancelButton = styled(Button)({
+   width: '5.4131rem',
+   height: '1.875rem',
+   borderRadius: '1.5rem',
+   gap: '0.5rem',
+})
+
+const AddnewButton = styled(Button)({
+   width: '7.9375rem',
+   height: '1.875rem',
+   borderRadius: '0.5rem',
+   border: '0.0625rem',
+   color: 'black',
+   background: '#D0D0D0',
+   boxSizing: 'border-box',
+   padding: '0.375rem, 0.875rem, 0.375rem, 1rem',
 })
