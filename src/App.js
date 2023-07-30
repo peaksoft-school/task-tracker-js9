546b8f27bf2693b99263cd5058d585dc1fe5e5a3
Zhypargul Kanybekova <jypargul06@gmail.com>
2023-07-31 00:18:42 +0600

    first commit  workspace modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { Workspaces } from './components/workspace/Workspace'
-
 function App() {
    return (
       <div>
          <h1>Task Trecker js-9</h1>
-         <Workspaces />
       </div>
    )
 }
