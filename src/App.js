3339e03437baa5566a548595b733a7bd3c3280cc
Ali <alisamatov106@gmail.com>
2023-07-31 13:11:38 +0600

    created storeFolder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { Column } from './components/column/Column'
+import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './assets/styles/theme'
+import { routes } from './routes/AppProvider'
 
 function App() {
    return (
-      <div>
-         <Column />
-      </div>
+      <>
+         <ToastContainer />
+         <ThemeProvider theme={theme}>
+            <RouterProvider router={routes} />
+         </ThemeProvider>
+      </>
    )
 }
 export default App
