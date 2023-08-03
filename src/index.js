c66ad3977981f3cec5fef4a488700f37e0d916c6
Zhypargul Kanybekova <jypargul06@gmail.com>
2023-08-04 00:10:43 +0600

    update workspacemodal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Workspaces } from './components/workspace/Workspace'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
+      <Workspaces />
       <App />
    </React.StrictMode>
 )
