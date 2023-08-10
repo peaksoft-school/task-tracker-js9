5abb160d25a994333916ab2ec986340dece19faf
airises <isaevasai7@gmail.com>
2023-08-10 16:08:19 +0600

    updated all All-Issues

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 // import { BrowserRouter } from 'react-router-dom'
-// import { ToastContainer } from 'react-toastify'
-// import { ThemeProvider } from '@mui/material'
-// import { RouterProvider } from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import { theme } from './assets/styles/theme'
-// import { routes } from './routes/AppProvider'
-// import { store } from './store'
-// import { injectStore } from './config/axiosInstance'
+import { ToastContainer } from 'react-toastify'
+import { ThemeProvider } from '@mui/material'
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { theme } from './assets/styles/theme'
+import { routes } from './routes/AppProvider'
+import { store } from './store'
+import { injectStore } from './config/axiosInstance'
 
-import { AllIssues } from './components/allIssues/AllIssues'
-
-// injectStore(store)
+injectStore(store)
 
 function App() {
    return (
-      // <Provider store={store}>
-      //    <ToastContainer />
-      //    <ThemeProvider theme={theme}>
-      //       <RouterProvider router={routes} />
-      //    </ThemeProvider>
-      // </Provider>
-      <div>
-         <AllIssues />
-      </div>
+      <Provider store={store}>
+         <ToastContainer />
+         <ThemeProvider theme={theme}>
+            <RouterProvider router={routes} />
+         </ThemeProvider>
+      </Provider>
    )
 }
 export default App
