ba695631d89ed8eba7326841b744ffaf14b71c32
Nurzhanov-Bekzat <bekzatnurzhanov888@gmail.com>
2023-08-15 14:31:08 +0600

    changed favorite interegation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
+import App from './App'
 import './index.css'
-import { Provider } from 'react-redux'
-import { Favourite } from './components/favourite/Favourite'
-import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
-      {/* <App /> */}
-      <Provider store={store}>
-         <Favourite />
-      </Provider>
+      <App />
    </React.StrictMode>
 )
