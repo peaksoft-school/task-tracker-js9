80fdd61870a8db68fc66ab1f212d346d43f7ff14
Amirbek Amiraev <amirbek.amiraev.kk@gmail.com>
2023-08-16 10:46:38 +0600

    second commit

--- src/routes/AppProvider.jsx
+++ src/routes/AppProvider.jsx
@@ -1,20 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { SignUpPage } from '../pages/SingUpage'
-// import { SignInPage } from '../pages/SignInPage'
+import { SignInPage } from '../pages/SignInPage'
 import { ResetPasswordPage } from '../pages/ResetPasswordPage'
 import { Headers } from '../components/header/Header'
 import { PrivateRoute } from './PrivateRoute'
 import { USER_ROLE } from '../utils/constants/authorization'
 import { Profile } from '../components/profile/Profile'
 import { Workspaces } from '../components/workspace/Workspace'
-import { InnerCard } from '../components/innerCard/InnerCard'
 
 export const routes = createBrowserRouter([
    {
       path: '/',
       element: (
          <PrivateRoute
-            component={<InnerCard />}
+            component={<SignInPage />}
             roles={[USER_ROLE.GUEST]}
             fallBacPath="/mainPage"
          />
